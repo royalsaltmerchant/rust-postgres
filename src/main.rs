85718b7d649e871a7906756ac7f789c21ffc10ea
Julian <jumpingafterrain@gmail.com>
2023-04-14 12:27:35 +0200

    init

---
+++ src/main.rs
@@ -0,0 +1,42 @@
+use sqlx::{postgres, Connection};
+mod objects;
+
+#[tokio::main]
+async fn main() {
+    println!("***** Wyrld *****\n\n");
+
+    let first_character = objects::Character::new(
+        String::from("Jules"),
+        22,
+        String::from("Warrior"),
+        String::from("Human"),
+    );
+
+    let first_monster = objects::Monster::new(String::from("Blork"), String::from("Mud"));
+
+    println!(
+        "First character: {}\nFirst Monster: {}",
+        first_character.name, first_monster.name
+    );
+
+    // DB
+    let mut conn = postgres::PgConnection::connect("postgresql://localhost/postgres")
+        .await
+        .unwrap();
+
+    let query_result = sqlx::query(
+        "insert into character (name, age, class, race, hp, exp) values ($1,$2,$3,$4,$5,$6);",
+    )
+    .bind(first_character.name)
+    .bind(first_character.age)
+    .bind(first_character.class)
+    .bind(first_character.race)
+    .bind(first_character.hp)
+    .bind(first_character.exp)
+    .execute(&mut conn)
+    .await
+    .unwrap();
+    println!("query result: {:?}", query_result);
+
+    println!("**** FINISHED ****")
+}
---
+++ src/objects/mod.rs
@@ -0,0 +1,33 @@
+pub struct Character {
+    pub name: String,
+    pub age: i32,
+    pub class: String,
+    pub race: String,
+    pub hp: i32,
+    pub exp: i32,
+}
+
+impl Character {
+    pub fn new(name: String, age: i32, class: String, race: String) -> Character {
+        Character {
+            name,
+            age,
+            class,
+            race,
+            hp: 30,
+            exp: 0,
+        }
+    }
+}
+
+pub struct Monster {
+    pub name: String,
+    pub kind: String,
+    pub hp: i32,
+}
+
+impl Monster {
+    pub fn new(name: String, kind: String) -> Monster {
+        Monster { name, kind, hp: 50 }
+    }
+}
